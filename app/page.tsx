8a2e7174bb95a60931fb528508fcf4dede110de9
Blue-Onion <rawatadityasingh963@gmail.com>
2026-08-09 01:24:58 +0530

    style: update Hero component typography, colors, and button styling while adding a background color to the main layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-[#e8eaef]">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,15 @@ const Hero = () => {
       className="w-screen h-screen flex flex-col justify-center items-center "
     >
       <div className="flex flex-col justify-center items-center ">
-        <h1 className="text-8xl font-bold flex gap-4">
-          <span>Harvest</span>
-          <span className="text-green-500">Global</span>
+        <h1 className=" text-6xl md:text-8xl font-bold flex gap-4">
+          <span className="text-emerald-600">Harvest</span>
+          <span className="text-orange-500">Global</span>
         </h1>
         <p className="">Invest in the Future of Food</p>
       </div>
       <div className="button flex gap-4 mt-8">
-        <button className="bg-green-500 text-white px-4 py-2 rounded-full hover:bg-green-600 transition-colors cursor-pointer">Explore Opportunities</button>
-        <button className="bg-white text-green-500 px-4 py-2 rounded-full hover:bg-green-100 transition-colors cursor-pointer">Contact Us</button>
+        <button className="bg-emerald-500 shadow-2xl shadow-emerald-500/40 text-white font-bold px-4 py-2 rounded-full hover:bg-green-600 transition-colors cursor-pointer text-md md:text-xl">Explore Opportunities</button>
+        <button className="bg-orange-500 shadow-2xl shadow-orange-500/40 text-white font-bold px-4 py-2 rounded-full transition-colors cursor-pointer">Contact Us</button>
       </div>
     </section>
   );
