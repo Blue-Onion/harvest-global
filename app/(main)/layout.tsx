849a77a8571224b1513fef98ef83a0243bc1524e
Blue-Onion <rawatadityasingh963@gmail.com>
2026-08-29 23:29:43 +0530

    feat: redesign connect page with full-screen background image and updated layout spacing

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="text-white mt-35 mx-auto container">{children}</div>
+      <div className="text-white mx-auto ">{children}</div>
     </>
   );
 }
